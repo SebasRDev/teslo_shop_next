f926a5428647717d61ffad719f75b2767237049a
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-07-28 07:48:39 -0500

    estrcutura de archivos, primer layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
-import type { NextPage } from 'next'
+import { Typography } from "@mui/material";
+import type { NextPage } from "next";
+import { ShopLayout } from "../components/layouts";
 
 const Home: NextPage = () => {
   return (
-    <h1>Teslo-shop</h1>
-  )
-}
+    <ShopLayout
+      title={"teslo-shop - Home"}
+      pageDescription={"Encuentra los mejores productos de Teslo aqui"}
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ marginBottom: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  );
+};
 
-export default Home
+export default Home;
