78701a897cd68d06479fed0f445d04941007cb25
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-07-29 23:21:29 -0500

    Carrito de compras elementos

---
+++ components/cart/CartList.tsx
@@ -0,0 +1,66 @@
+import NextLink from "next/link";
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import { initialData } from "../../database/products";
+import { ItemCounter } from "../ui";
+
+const prodcutsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+export const CartList = () => {
+  return (
+    <>
+      {prodcutsInCart.map((product) => (
+        <Grid container spacing={2} sx={{ mb: 1 }} key={product.slug}>
+          <Grid item xs={3}>
+            {/**TODO: LLevar a la pagina del producto */}
+            <NextLink href={"/product/slug"} passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: "5px" }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography variant="body1">
+                Talla: <strong>M</strong>
+              </Typography>
+
+              {/**Condicional */}
+              <ItemCounter />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">${product.price}</Typography>
+
+            {/**Editable */}
+            <Button variant="text" color="secondary">Eliminar</Button>
+          </Grid>
+        </Grid>
+      ))}
+    </>
+  );
+};
