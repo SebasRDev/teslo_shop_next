b00faaa73f6704d31c1a73432f7955b11edbe733
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-07-30 01:06:04 -0500

    pagina de registro

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -3,30 +3,33 @@ import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../../components/layouts";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <AuthLayout title={"Ingresar"}>
       <Box sx={{ width: 350, padding: "10px 20px" }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Typography variant="h1" component="h1">
-              Iniciar Sesión
+              Registrarse
             </Typography>
           </Grid>
 
           <Grid item xs={12}>
-            <TextField label="correo" fullWidth variant="outlined"/>
+            <TextField label="Nombre" fullWidth variant="outlined"/>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Correo" fullWidth variant="outlined"/>
           </Grid>
           <Grid item xs={12}>
             <TextField label="Contraseña" fullWidth variant="outlined" type="password"/>
           </Grid>
           <Grid item xs={12}>
-            <Button color="secondary" className="circular-btn" size="large" fullWidth>Ingresar</Button>
+            <Button color="secondary" className="circular-btn" size="large" fullWidth>Registrarse</Button>
           </Grid>
 
           <Grid item xs={12} display="flex" justifyContent="center">
-            <NextLink  href='/auth/register' passHref>
-              <Link underline='always'>No tienes cuenta? registrate</Link>
+            <NextLink  href='/auth/login' passHref>
+              <Link underline='always'>ya tienes cuenta? Ingresa</Link>
             </NextLink>
           </Grid>
         </Grid>
@@ -35,4 +38,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
