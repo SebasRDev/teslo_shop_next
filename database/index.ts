37e55116facc819eb56f4612b5f1512fd8f3ac35
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-07-30 23:02:56 -0500

    creacion de imagen en docker, coneccion y subida de test data a la bd

---
+++ database/index.ts
@@ -0,0 +1,4 @@
+export * as db from './db';
+
+//BD de prueba
+export * as seedDB from './products';
---
+++ models/Product.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, model, Model } from "mongoose";
+import { IProduct } from "../interfaces";
+
+const productSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+          message: "{VALUE} no es un tamaño válido",
+        },
+      },
+    ],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
+    type: {
+      type: String,
+      emun: {
+        values: ["shirts", "pants", "hoodies", "hats"],
+        message: "{VALUE} no es un tipo valido",
+      },
+    },
+    gender: {
+      type: String,
+      emun: {
+        values: ["men", "women", "kid", "unisex"],
+        message: "{VALUE} no es un genero valido",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//TODO: Crear indice de Mongo
+
+const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema);
+
+export default Product;
